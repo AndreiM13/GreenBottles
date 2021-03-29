9184142c29573cb3da352813de63bddd1b6bccdb
AndreiM13 <48647892+AndreiM13@users.noreply.github.com>
2021-03-29 11:40:16 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int Greenbottles =10;
+
+  cout<<" There were"<<" "<<Greenbottles<<" "<<"green bottles"<<endl;
+
+  Greenbottles--;
+   cout<<" There were"<<" "<<Greenbottles<<" "<<"green bottles";
+    return 0;
+}
